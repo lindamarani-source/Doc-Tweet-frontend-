5829a6f6d0f606dcf28a957037daac6dc6da108a
lindamarani-source <linda.marani@studentmoringaschool.com>
2026-07-31 08:55:43 +0300

    Fix App.css import and Link import in login.jsx

--- Doc-Tweet/src/App.jsx
+++ Doc-Tweet/src/App.jsx
@@ -6,7 +6,6 @@ import Signup from './pages/signup';
 import Profile from './pages/profile';
 import Posts from './pages/posts';
 import Navbar from './components/Navbar';
-import './App.css';
 
 function App() {
   return (
